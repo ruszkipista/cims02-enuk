0a3f333256bd719fa679ee953303739b7e45e99d
ruszkipista <oi5672@gmail.com>
2021-02-10 13:37:38 +0000

    put the sun piece on board, dynamic  position

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,8 +29,10 @@ function getGameMaterials(){
     { type: 'piece-figure', name: 'green', count: 4 },
     { type: 'piece-figure', name: 'orange', count: 4 },
     { type: 'piece-figure', name: 'purple', count: 4 },
-    { type: 'piece-sun', name: 'piece-sun.png', count: 1 },
-    { type: 'piece-board', name: 'enuk-board-front.jpg', count: 1 },
+    { type: 'piece-sun', name: 'piece-sun.png', count: 1, length: 0.08556 },
+    { type: 'piece-board', name: 'enuk-board-front.jpg', count: 1, 
+      sunCenters: [[0.144385,0.074866]],
+    },
   ]
 }
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -43,9 +45,10 @@ function shuffleArrayInplace(arr) {
 }
 function generateTiles(isRandom) {
     let gameMaterials = getGameMaterials();
-    let pathName = "";
+    let path;
     let backtileName = "";
     let pieceBoard;
+    let pieceSun;
     // assemble tiles
     tiles = [];
     for (let piece of gameMaterials) {
@@ -54,7 +57,7 @@ function generateTiles(isRandom) {
                 tiles.push({name:piece.name});
             };
         } else if (piece.type === 'path-image') {
-            pathName = piece.name;
+            path = piece;
         } else if (piece.type === 'tileback') {
             backtileName = piece.name;
         } else if (piece.type === 'piece-board') {
@@ -64,10 +67,16 @@ function generateTiles(isRandom) {
         }
     };
     // put game board and sun piece in place
-    document.getElementById('board').innerHTML = `
-      <img id="${pieceBoard.type}" src="${pathName}${pieceBoard.name}" alt="game board">           
-      <img id="${pieceSun.type}" src="${pathName}${pieceSun.name}" alt="game piece sun">
-    `;            
+    let boardElement = document.getElementById('board');
+    boardElement.innerHTML = `
+      <img id="${pieceBoard.type}" src="${path.name}${pieceBoard.name}" alt="game board">           
+      <img id="${pieceSun.type}" src="${path.name}${pieceSun.name}" alt="game piece sun">
+    `;
+    let boardWidth = document.getElementById(pieceBoard.type).naturalWidth;
+    let sunLength = boardWidth * pieceSun.length;
+    document.documentElement.style.setProperty('--piece-sun-length', `${sunLength}px`);
+    document.documentElement.style.setProperty('--piece-sun-fromtop', `${boardWidth * pieceBoard.sunCenters[0][0] - sunLength/2}px`);
+    document.documentElement.style.setProperty('--piece-sun-fromleft', `${boardWidth * pieceBoard.sunCenters[0][1] - sunLength/2}px`);
 
     if (isRandom) { shuffleArrayInplace(tiles) };
     let tilesElement = document.getElementById('tiles');
@@ -78,13 +87,13 @@ function generateTiles(isRandom) {
         tileElement.innerHTML = `
         <div class="tile-inner">
           <div class="tile-front">
-            <img src="${pathName}${materialTypeTileBack}-${backtileName}.jpg" alt="game tile back">
+            <img src="${path.name}${materialTypeTileBack}-${backtileName}.jpg" alt="game tile back">
           </div>
           <div class="tile-front">
             ${(isTest) ? tile.name : ""}
           </div>
           <div class="tile-back">
-            <img src="${pathName}${materialTypeTileFace}-${tile.name}.jpg" alt="game tile face">
+            <img src="${path.name}${materialTypeTileFace}-${tile.name}.jpg" alt="game tile face">
           </div>
         </div>
         `
