b212a7fca9f3492634c4727d2f00474ada3d0e30
ruszkipista <oi5672@gmail.com>
2021-02-13 16:42:47 +0000

    refactor gameStato to gameContorller

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,42 @@
+// UTILITIES
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+// learnt random shuffling from here https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArrayInplace(arr) {
+  let j;
+  for (let i = arr.length - 1; i > 0; i--) {
+    j = getRandomInt(i + 1);
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
+function setVisibilityOfElement(elementID, isVisible) {
+  const element = document.getElementById(elementID);
+  if (isVisible) {
+    element.style.visibility = 'visible';
+  } else {
+    element.style.visibility = 'hidden';
+  }
+}
+
 // generate tiles
 document.addEventListener('DOMContentLoaded', function () {
-  gameState.setupPlayers(4, true);
+  gameController.setupPlayers(4, true);
   generateGameBoard();
   setBoardPiecesPosition();
-  gameState.players[0].score = 30;
-  gameState.players[1].score = 31;
-  gameState.players[2].score = 2;
-  gameState.players[3].score = 2;
+  gameController.players[0].score = 30;
+  gameController.players[1].score = 31;
+  gameController.players[2].score = 2;
+  gameController.players[3].score = 2;
   setScoresOnBoard();
-  gameState.players[3].score++;
+  gameController.players[3].score++;
   setScoresOnBoard();
-  gameState.whosRound = 0;
+  gameController.whosMove = 0;
 });
 // reposition the sun piece after window resize or change between landscape and portrait
 window.addEventListener('resize', function () { setBoardPiecesPosition(); });
 
+// object GAMEMATERIALS
 const gameMaterials = {
   imagePath: './assets/img/',
   nameIgloo: 'igloo',
@@ -85,6 +107,7 @@ const gameMaterials = {
   },
 }
 
+// class TILE
 class Tile {
   constructor(id, name, filename) {
     this.id = id;
@@ -108,7 +131,7 @@ class Tile {
             <img src="${path}${backtileName}" alt="game tile back">
           </div>
           <div class="tile-front">
-            ${(gameState.isTest) ? this.name : ""}
+            ${(gameController.isTest) ? this.name : ""}
           </div>
           <div class="tile-back">
             <img src="${path}${this.filename}" alt="game tile ${this.name}">
@@ -132,13 +155,14 @@ class Tile {
   }
 }
 
-const gameState = {
+// object GAMECONTROLLER
+const gameController = {
   isTest: null,
   tilesOnTable: null,
   tilesOnIgloo: null,
   sunPosition: null,
   round: null,
-  whosRound: null,
+  whosMove: null,
   players: null,
 
   setupPlayers: function (numberOfPlayers, isTest) {
@@ -154,7 +178,7 @@ const gameState = {
         name: pieceFigures[i].name,
         filename: pieceFigures[i].filename,
         figures: [],
-        // generate ID for each Tile Stack (score) - one for each player
+        // generate ID for each Tile Stack (score) - one stack per player
         tileStackID: `tiles-stack-player${i}`,
         tilesInStack: [],
         score: 0,
@@ -179,25 +203,29 @@ const gameState = {
     }
     if (!this.isTest) { shuffleArrayInplace(this.tiles) };
   },
+
   findTileOnTable: function (idOnTable) {
     // learnt "find" from https://usefulangle.com/post/3/javascript-search-array-of-objects
-    let tile = gameState.tilesOnTable.find(function (element, index) {
+    let tile = gameController.tilesOnTable.find(function (element, index) {
       if (element.idOnTable === idOnTable) return true;
     })
     return tile;
   },
 
   removeTileFromTable: function (tile) {
-    setVisibilityOfElement(tile.idOnTable, false);
+    let tileIndex = this.tilesOnTable.findIndex(function (element, index) {
+      if (element.idOnTable === tile.idOnTable) return true;
+    })
+    this.tilesOnTable.splice(tileIndex,1);
     return tile;
   },
 
   addTileToIgloo: function (tile) {
-    setVisibilityOfElement(tile.idOnIgloo, true);
+    this.tilesOnIgloo.push(tile);
   },
 
   handleTileClickOnTable: function (tileIdOnTable, isTopLeft, isTopRight) {
-    if (this.whosRound !== 0) {
+    if (this.whosMove !== 0) {
       return;
     }
     const tile = this.findTileOnTable(tileIdOnTable);
@@ -205,33 +233,23 @@ const gameState = {
       tile.flipOnTable(isTopLeft, isTopRight);
       switch (tile.name) {
         case gameMaterials.nameIgloo:
-          setInterval(function(){ 
-            gameState.removeTileFromTable(tile);
-            gameState.addTileToIgloo(tile);
+          this.removeTileFromTable(tile);
+          this.addTileToIgloo(tile);
+          setInterval(function(){
+            setVisibilityOfElement(tile.idOnTable, false);
+            setVisibilityOfElement(tile.idOnIgloo, true);
           }, 2000);
           break;
         case gameMaterials.nameReindeer:
           const pieceBoard = gameMaterials.getBoardPiece();
-          if (gameState.sunPosition < pieceBoard.sunCenters.length - 1) {
-            ++gameState.sunPosition;
+          if (this.sunPosition < pieceBoard.sunCenters.length - 1) {
+            this.sunPosition++;
             setBoardPiecesPosition();
           };
           break;
       }
     }
   },
-};
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-// learnt random shuffling from here https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffleArrayInplace(arr) {
-  let j;
-  for (let i = arr.length - 1; i > 0; i--) {
-    j = getRandomInt(i + 1);
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
 }
 
 function generateGameBoard() {
@@ -250,7 +268,7 @@ function generateGameBoard() {
   boardPiecesHTML += `<img id="${pieceSun.id}" src="${path}${pieceSun.filename}" alt="game piece sun">`;
   // add hidden IGLOO TILES to the middle of the board
   boardPiecesHTML += `<div id="tiles-igloo">`;
-  for (let tile of gameState.tiles) {
+  for (let tile of gameController.tiles) {
     if (tile.name === gameMaterials.nameIgloo) {
       boardPiecesHTML += `<img id="${tile.idOnIgloo}" class="tile-igloo" 
                             src="${path}${tile.filename}"
@@ -261,15 +279,15 @@ function generateGameBoard() {
   boardPiecesHTML += `</div>`;
 
   // add TILE STACKS and FIGURES for all players on the board
-  for (let i = 0; i < gameState.players.length; i++) {
-    boardPiecesHTML += `<div id="${gameState.players[i].tileStackID}" class="tiles-stack tiles-stack-player${i}"></div>`;
+  for (let i = 0; i < gameController.players.length; i++) {
+    boardPiecesHTML += `<div id="${gameController.players[i].tileStackID}" class="tiles-stack tiles-stack-player${i}"></div>`;
     boardPiecesHTML += `<div id="figures-player${i}" 
                              class="figures-group tiles-stack-player${i}" 
-                             style="border-top-color:${gameState.players[i].name}">`;
-    for (let figure of gameState.players[i].figures) {
+                             style="border-top-color:${gameController.players[i].name}">`;
+    for (let figure of gameController.players[i].figures) {
       boardPiecesHTML += `<img id="${figure.id}" class="figure-on-board" 
-                            src="${path}${gameState.players[i].filename}"
-                            alt="game figure ${gameState.players[i].name}">`;
+                            src="${path}${gameController.players[i].filename}"
+                            alt="game figure ${gameController.players[i].name}">`;
     }
     boardPiecesHTML += `</div>`;
   }
@@ -280,10 +298,10 @@ function generateGameBoard() {
   let tilesElement = document.getElementById('tiles');
   // assemble tiles
   while (true) {
-    let tile = gameState.tiles.shift();
+    let tile = gameController.tiles.shift();
     if (tile === undefined) { break }
     tileElement = tile.placeOnTable(false);
-    gameState.tilesOnTable.push(tile);
+    gameController.tilesOnTable.push(tile);
     tilesElement.appendChild(tileElement);
   }
 }
@@ -296,11 +314,11 @@ function setBoardPiecesPosition() {
   const sunLength = boardWidth * pieceBoard.sunLength;
   document.documentElement.style.setProperty('--piece-sun-length', `${sunLength}px`);
   document.documentElement.style.setProperty('--piece-sun-fromtop',
-    `${boardWidth * pieceBoard.sunCenters[gameState.sunPosition][0] - sunLength / 2}px`);
+    `${boardWidth * pieceBoard.sunCenters[gameController.sunPosition][0] - sunLength / 2}px`);
   document.documentElement.style.setProperty('--piece-sun-fromleft',
-    `${boardLeftOffset + boardWidth * pieceBoard.sunCenters[gameState.sunPosition][1] - sunLength / 2}px`);
+    `${boardLeftOffset + boardWidth * pieceBoard.sunCenters[gameController.sunPosition][1] - sunLength / 2}px`);
   document.documentElement.style.setProperty('--piece-sun-rotate',
-    `${gameState.sunPosition * 130}deg`);
+    `${gameController.sunPosition * 130}deg`);
 
   const iglooLength = boardWidth * pieceBoard.iglooLength;
   document.documentElement.style.setProperty('--piece-igloo-length', `${iglooLength}px`);
@@ -314,7 +332,7 @@ function setBoardPiecesPosition() {
   document.documentElement.style.setProperty('--figure-onboard-width', `${figureWidth}px`);
   document.documentElement.style.setProperty('--tile-edge-width', `${figureWidth * 4}px`);
   document.documentElement.style.setProperty('--tiles-stack-height', `${boardWidth * pieceBoard.figuresOnBoardFromTop}px`);
-  for (let i = 0; i < gameState.players.length; i++) {
+  for (let i = 0; i < gameController.players.length; i++) {
     document.documentElement.style.setProperty('--board-figures-fromtop',
       `${boardWidth * pieceBoard.figuresOnBoardFromTop}px`);
     document.documentElement.style.setProperty(`--tiles-stack-fromleft${i}`,
@@ -325,23 +343,14 @@ function setBoardPiecesPosition() {
 function handleTileClick(event) {
   let isTopRight = event.layerY < event.layerX;
   let isTopLeft = event.layerY < (event.currentTarget.offsetWidth - event.layerX);
-  gameState.handleTileClickOnTable(event.currentTarget.id, isTopLeft, isTopRight);
-}
-
-function setVisibilityOfElement(elementID, isVisible) {
-  const element = document.getElementById(elementID);
-  if (isVisible) {
-    element.style.visibility = 'visible';
-  } else {
-    element.style.visibility = 'hidden';
-  }
+  gameController.handleTileClickOnTable(event.currentTarget.id, isTopLeft, isTopRight);
 }
 
 function setScoresOnBoard() {
   const tileEdges = gameMaterials.getTileEdges();
   let tilesHTML = null;
   let stackElement = null;
-  for (let player of gameState.players) {
+  for (let player of gameController.players) {
     stackElement = document.getElementById(player.tileStackID);
     tilesHTML = stackElement.innerHTML;
     if (player.score > 0) {
